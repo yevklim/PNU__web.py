cd6af2b61c58f549e051c0f12aff287237b45995
Yevhenii <64846678+yevklim@users.noreply.github.com>
2023-10-06 21:33:22 +0300

    Lab 1: finished task 1.2

--- lab1/client.py
+++ lab1/client.py
@@ -5,7 +5,7 @@ import shared
 import socket
 
 def main():
-    with socket.socket() as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(shared.SERVER_ADDRESS)
         print(f"[CLIENT] Connected with server")
 
@@ -13,6 +13,14 @@ def main():
 
         print(f"[CLIENT] Sending message to the server...")
         s.sendall(msg.encode())
+
+        print(f"[CLIENT] Waiting for response from the server...")
+        while 1:
+            data = s.recv(shared.BUFSIZE)
+            if not data:
+                break
+            print(f"[CLIENT] Received: {data}")
+
 
 if __name__ == "__main__":
     main()
--- lab1/server.py
+++ lab1/server.py
@@ -4,12 +4,13 @@ import shared
 
 import socket
 import datetime
+import time
 
 def main():
     with socket.socket() as s:
         print("[SERVER] Socket created")
         s.bind(shared.SERVER_ADDRESS)
-        s.listen()
+        s.listen(10)
         print("[SERVER] Socket starts listening")
 
         while 1:
@@ -17,11 +18,22 @@ def main():
             with connection:
                 print(f"[SERVER] Connected with client {client_address}")
                 while 1:
-                    data = connection.recv(1024)
+                    data = connection.recv(shared.BUFSIZE)
                     if not data:
                         break
                     timestamp = datetime.datetime.now()
                     print(f"[SERVER] Received message from the client:\n\t{timestamp}\n\t{data}\n")
+
+                    time.sleep(5)
+
+                    # Змініть код так, щоб сервер відповідав ... із ... перевіркою (використати розмір даних),
+                    # що всі дані успішно відправлено. І далі закривав з’єднання з клієнтом.
+                    data_len = len(data)
+                    sent = 0
+                    while sent < data_len:
+                        sent += connection.send(data[sent:sent+shared.BUFSIZE])
+                    break
+                connection.shutdown(socket.SHUT_RD)
 
 if __name__ == "__main__":
     main()
