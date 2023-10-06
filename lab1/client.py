919c304e3d5b671beead5003e479229f7e0252eb
Yevhenii <64846678+yevklim@users.noreply.github.com>
2023-10-06 20:33:22 +0300

    Lab 1: finished task 1.1

--- lab1/client.py
+++ lab1/client.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
+import shared
+
 import socket
 
 def main():
-    pass
+    with socket.socket() as s:
+        s.connect(shared.SERVER_ADDRESS)
+        print(f"[CLIENT] Connected with server")
+
+        msg = input("[CLIENT] Provide a message: ")
+
+        print(f"[CLIENT] Sending message to the server...")
+        s.sendall(msg.encode())
 
 if __name__ == "__main__":
     main()
--- lab1/server.py
+++ lab1/server.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 
+import shared
+
 import socket
+import datetime
 
 def main():
-    pass
+    with socket.socket() as s:
+        print("[SERVER] Socket created")
+        s.bind(shared.SERVER_ADDRESS)
+        s.listen()
+        print("[SERVER] Socket starts listening")
+
+        while 1:
+            connection, client_address = s.accept()
+            with connection:
+                print(f"[SERVER] Connected with client {client_address}")
+                while 1:
+                    data = connection.recv(1024)
+                    if not data:
+                        break
+                    timestamp = datetime.datetime.now()
+                    print(f"[SERVER] Received message from the client:\n\t{timestamp}\n\t{data}\n")
 
 if __name__ == "__main__":
     main()
