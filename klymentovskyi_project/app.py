6bc015fc5f998a72ff495a0b877375ca9432fad3
Yevhenii <64846678+yevklim@users.noreply.github.com>
2023-10-28 19:32:57 +0300

    Lab 3: finished

---
+++ klymentovskyi_project/app.py
@@ -0,0 +1,32 @@
+#!./.env/bin/python
+
+from os import uname
+from flask import Flask, request, render_template, redirect, url_for
+from data import skills_list, projects_list
+system_info=f"{uname().sysname} {uname().release} {uname().machine}"
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html", system_info=system_info, user_agent=request.user_agent)
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html", projects_list=projects_list)
+
+@app.route('/skills/')
+@app.route('/skills/<int:idx>')
+def skills(idx=None):
+    if idx is not None:
+        return render_template("skill.html", skills_list=skills_list, idx=idx)
+    else:
+        return render_template("skills.html", skills_list=skills_list)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ klymentovskyi_project/data.py
@@ -0,0 +1,46 @@
+skills_list = [
+    {
+        "title": "Software Development",
+        "experience": "7 years total (2yrs professional)", "subskills": []
+    },
+    {
+        "title": "Desktop Application Development",
+        "experience": "6 years total (2yrs professional)", "subskills": [
+            "C/C++", "Python", "Javascript", "Qt QML", "Electron"
+        ]
+    },
+    {
+        "title": "Web Applications Front-End Development",
+        "experience": "6 years total (2yrs professional)", "subskills": [
+            "Javascript", "CSS", "HTML", "Preact", "SPA (Single Page Applications)",
+        ]
+        },
+    {
+        "title": "ActionScript3 Desktop Application Development",
+        "experience": "2 years professional", "subskills": []
+        },
+    {
+        "title": "Web Crawling & Scraping", "experience":
+        "4 years total (2yrs professional)", "subskills": []
+    },
+]
+projects_list = [
+    {
+        "title": "\"ComixReader\" Desktop Application",
+        "description": """
+        A comics gallery application that lets user read comics's from different web sites.
+        It provide a lot of features to ensure pleasant user experience.
+        """,
+        "technologies": ["C++", "Qt QML", "Web Crawling & Scraping"]
+    },
+    {
+        "title": "Project Alpha",
+        "description": "(Encrypted) XtRlmvQpZbWn9KjA7zYsGxUc3FtV2I8hL1OyEw6qPd5oN4rSfRvWxJzQkTl5iGpHsFyDmBn7o1eU3qY8f2A6Xc9LwOuVdZgKbPtNrMjS4hI",
+        "technologies": []
+    },
+    {
+        "title": "Project Gamma",
+        "description": "(Encrypted) KtV7zWfXj9rMgA5hYpEiRbUcLmNqD2w3P6oQlSvT1xZsGyJn8HyFsHpGi5lTkQzJxWvRfSr4No5dPq6wEyO1Lh8I2VtF3cUxGsYz7A",
+        "technologies": []
+    },
+]
